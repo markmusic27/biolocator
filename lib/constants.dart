6dd002f9d9666d8154c37e297cdf77201c2c7ddd
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-12-09 20:11:53 -0600

    feat(core): Add basic welcome page

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kHeader1TextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w700,
+  color: Colors.white,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.w700,
+  color: Colors.white,
+);
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,75 @@
 import 'package:flutter/material.dart';
+import "package:biolocator/constants.dart";
 
 class WelcomeScreen extends StatelessWidget {
   static String id = "welcome_screen";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          image: DecorationImage(
+            image: NetworkImage(
+                "https://www.lefthudson.com/wp-content/uploads/2019/11/blue-iphone-background-awesome-blue-blur-iphone-6-plus-wallpaper-abstract-iphone-6-plus-wallpapers-inspiration-of-blue-iphone-background.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to Biolocator.",
+                style: kHeader1TextStyle,
+                textAlign: TextAlign.center,
+              ),
+              ButtonDisplay(),
+              Padding(
+                padding: EdgeInsets.only(top: 32.0),
+                child: Text(
+                  "Created by Mark Music and Filippo Fonseca. \nBiology 9 Summative Assesment (Criterion D).",
+                  style: kHeader1TextStyle.copyWith(
+                      fontSize: 15.0, color: Colors.black, height: 1.75),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 50.0),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: GestureDetector(
+              child: Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: Colors.black,
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Initialize",
+                          style: kButtonTextStyle, textAlign: TextAlign.center),
+                    )),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
